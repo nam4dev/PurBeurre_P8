76e9f5f457aad778d014b26f4223cc0cfffb1206
Elwaze <eloise.lamontagne@gmail.com>
2020-07-22 16:25:37 +0200

    problems with selenium settings

--- pur_beurre/tests/functional_tests/func_tests.py
+++ pur_beurre/tests/functional_tests/func_tests.py
@@ -1,16 +1,70 @@
+from importlib import import_module
+
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from django.conf import settings
+
+#
+# def create_session_store():
+#     """ Creates a session storage object. """
+#
+#     # from django.utils.importlib import import_module
+#     engine = import_module(settings.SESSION_ENGINE)
+#     # Implement a database session store object that will contain the session key.
+#     store = engine.SessionStore()
+#     store.save()
+#     return store
 
 
 class GeneralTestCase(LiveServerTestCase):
     """
     Parent class to setup the selenium tests.
     """
+    selenium = None
 
-    def setUp(self):
-        self.selenium = webdriver.Chrome('functional_tests/chromedriver.exe')
-        super(GeneralTestCase, self).setUp()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = webdriver.Firefox(executable_path=r'.\tests\functional_tests\geckodriver.exe')
+        cls.selenium.implicitly_wait(10)
 
-    def tearDown(self):
-        self.selenium.quit()
-        super(GeneralTestCase, self).tearDown()
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+
+    # def setUp(self):
+    #     super(GeneralTestCase, self).setUp()
+    #     self.selenium = webdriver.Chrome(executable_path=r'.\tests\functional_tests\chromedriver.exe')
+    #     self.selenium.implicitly_wait(3)
+    #
+    # def tearDown(self):
+    #     super(GeneralTestCase, self).tearDown()
+    #     self.selenium.implicitly_wait(3)
+    #     self.selenium.quit()
+
+    # def test_welcome_page(self):
+    #     # pdb.set_trace()
+    #     # Create a session storage object.
+    #     session_store = create_session_store()
+    #     # In pdb, you can do 'session_store.session_key' to view the session key just created.
+    #
+    #     # Create a session object from the session store object.
+    #     session_items = session_store
+    #
+    #     # Add a session key/value pair.
+    #     session_items['uid'] = 1
+    #     session_items.save()
+    #
+    #     # Go to the correct domain.
+    #     self.selenium.get(self.live_server_url)
+    #
+    #     # Add the session key to the cookie that will be sent back to the server.
+    #     self.selenium.add_cookie({'name': settings.SESSION_COOKIE_NAME, 'value': session_store.session_key})
+    #     # In pdb, do 'self.selenium.get_cookies() to verify that it's there.'
+    #
+    #     # The client sends a request to the view that's expecting the session item.
+    #     self.selenium.get(self.live_server_url + '/signup/')
+    #     body = self.selenium.find_element_by_tag_name('body')
+    #     self.assertIn('Welcome', body.text)
--- pur_beurre/tests/functional_tests/purbeurre_user/tests/test_user.py
+++ pur_beurre/tests/functional_tests/purbeurre_user/tests/test_user.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from selenium.webdriver.common.keys import Keys
 
 from tests.functional_tests.func_tests import GeneralTestCase
@@ -6,7 +7,7 @@ from tests.functional_tests.func_tests import GeneralTestCase
 class AccountTestCase(GeneralTestCase):
 
     def setUp(self):
-        GeneralTestCase.setUp(self)
+        super().setUp()
         selenium = self.selenium
         # find the form element
         self.id_username = selenium.find_element_by_xpath('//label[@for="id_username"]')
@@ -79,7 +80,7 @@ class AccountTestCase(GeneralTestCase):
         """
 
         # Opening the link we want to test
-        self.selenium.get('http://127.0.0.1:8000/user/connection')
+        self.selenium.get('/user/connection')
         # Fill the form with data
         self.id_username.send_keys('connection@selenium.com')
         self.id_password.send_keys('not_selenium_test')
@@ -106,3 +107,28 @@ class AccountTestCase(GeneralTestCase):
 
         # check the returned result
         assert 'Utilisateur inconnu ou mauvais de mot de passe.' in self.selenium.page_source
+
+    # def test_welcome_page(self):
+    #     # pdb.set_trace()
+    #     # Create a session storage object.
+    #     session_store = create_session_store()
+    #     # In pdb, you can do 'session_store.session_key' to view the session key just created.
+    #
+    #     # Create a session object from the session store object.
+    #     session_items = session_store
+    #
+    #     # Add a session key/value pair.
+    #     session_items['uid'] = 1
+    #     session_items.save()
+    #
+    #     # Go to the correct domain.
+    #     self.selenium.get(self.live_server_url)
+    #
+    #     # Add the session key to the cookie that will be sent back to the server.
+    #     self.selenium.add_cookie({'name': settings.SESSION_COOKIE_NAME, 'value': session_store.session_key})
+    #     # In pdb, do 'self.selenium.get_cookies() to verify that it's there.'
+    #
+    #     # The client sends a request to the view that's expecting the session item.
+    #     self.selenium.get(self.live_server_url + '/signup/')
+    #     body = self.selenium.find_element_by_tag_name('body')
+    #     self.assertIn('Welcome', body.text)
